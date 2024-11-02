5d99c62bdfb357c43275334a9b41ee8101103790
Chinmay Patil <patilchinmay01@gmail.com>
2024-11-02 09:55:23 +0900

    Update .aliases.sh

--- .aliases.sh
+++ .aliases.sh
@@ -6,11 +6,14 @@ alias kn='kubens'
 alias activate='source ./.venv/bin/activate'
 
 # Bat (better cat)
-alias cat='bat'
+alias cat='bat --paging=never --color=always --style=plain --theme=Nord'
 
 # ---- Eza (better ls) -----
 alias ls="eza --color=always --long --git --icons=always --all --group-directories-first"
 alias ll="eza --color=always --long --git --icons=always --all --group-directories-first"
+
+# ---- zoxide (better cd) -----
+alias cd='z'
 
 # Reload config
 alias reload='source ~/.zshrc'
